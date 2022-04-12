df3e68de88fb491d227dd087d8c714a8ee300a8e
HtHab <habteweldht@gmail.com>
2022-04-12 13:20:36 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "./axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import Video from "./Video";
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    async function fetchPost() {
+      const response = await axios.get("/v2/posts");
+      setVideos(response.data);
+      return response;
+    }
+    fetchPost();
+  }, []);
+
+  console.log(videos);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__videos">
+        {videos.map(
+          ({ url, channel, messages, shares, likes, song, description }) => (
+            <Video
+              url={url}
+              channel={channel}
+              shares={shares}
+              likes={likes}
+              song={song}
+              description={description}
+              messages={messages}
+            />
+          )
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/Video.js
@@ -0,0 +1,35 @@
+import React, { useRef, useState } from "react";
+import "./Video.css";
+import VideoFooter from "./VideoFooter";
+import VideoSidebar from "./VideoSidebar";
+
+function Video({ url, channel, description, song, likes, messages, shares }) {
+  const videoRef = useRef(null);
+  const [play, setPlay] = useState(false);
+
+  const onVideoPress = () => {
+    if (play) {
+      videoRef.current.pause();
+      setPlay(false);
+    } else {
+      videoRef.current.play();
+      setPlay(true);
+    }
+  };
+
+  return (
+    <div className="video">
+      <video
+        className="video__player"
+        ref={videoRef}
+        onClick={onVideoPress}
+        loop
+        src={url}
+      ></video>
+      <VideoFooter channel={channel} description={description} song={song} />
+      <VideoSidebar likes={likes} messages={messages} shares={shares} />
+    </div>
+  );
+}
+
+export default Video;
